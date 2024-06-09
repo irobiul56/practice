654235023b8d13a9985aef18d142c0d0b3b3f166
Robiul Islam <irobiul56@gmail.com>
2024-06-09 16:10:27 +0600

    vue practice

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const location = ref('Dhaka')
+
+function changelocation(newlocation){
+  location.value = newlocation
+}
+
+</script>
+
+<template>
+    <div>
+        <button @click="changelocation('Dhaka')">Dhaka</button>
+        <button @click="changelocation('Rangpur')">Rangpur</button>
+        <button @click="changelocation('Khulna')">Khulna</button><br><br>
+        <p>Your Location is: {{ location }} </p>
+    </div>
+</template>
+    
+<style scoped>
+
+</style>
---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+
+const data = [
+  {
+    image:'https://img.freepik.com/free-photo/smiling-young-male-professional-standing-with-arms-crossed-while-making-eye-contact-against-isolated-background_662251-838.jpg?t=st=1712896217~exp=1712899817~hmac=4889f4f7f534f3049e2f77fbdb49d65bd0f997c7d15dbd67d7f98de037cb7cd6&w=740',
+    title: 'Smiling young male professional standing',
+    link:'https://www.freepik.com/free-photo/smiling-young-male-professional-standing-with-arms-crossed-while-making-eye-contact-against-isolated-background_27507898.htm#fromView=search&page=1&position=0&uuid=6c210ec5-51d1-4315-885f-cc69ba2682b5',
+  },
+
+  {
+    image:'https://img.freepik.com/free-photo/smiling-young-male-professional-standing-with-arms-crossed-while-making-eye-contact-against-isolated-background_662251-838.jpg?t=st=1712896217~exp=1712899817~hmac=4889f4f7f534f3049e2f77fbdb49d65bd0f997c7d15dbd67d7f98de037cb7cd6&w=740',
+    title: 'Smiling young male professional standing',
+    link:'https://www.freepik.com/free-photo/smiling-young-male-professional-standing-with-arms-crossed-while-making-eye-contact-against-isolated-background_27507898.htm#fromView=search&page=1&position=0&uuid=6c210ec5-51d1-4315-885f-cc69ba2682b5',
+  },
+
+  {
+    image:'https://img.freepik.com/free-photo/smiling-young-male-professional-standing-with-arms-crossed-while-making-eye-contact-against-isolated-background_662251-838.jpg?t=st=1712896217~exp=1712899817~hmac=4889f4f7f534f3049e2f77fbdb49d65bd0f997c7d15dbd67d7f98de037cb7cd6&w=740',
+    title: 'Smiling young male professional standing',
+    link:'https://www.freepik.com/free-photo/smiling-young-male-professional-standing-with-arms-crossed-while-making-eye-contact-against-isolated-background_27507898.htm#fromView=search&page=1&position=0&uuid=6c210ec5-51d1-4315-885f-cc69ba2682b5',
+  },
+
+  {
+    image:'https://img.freepik.com/free-photo/smiling-young-male-professional-standing-with-arms-crossed-while-making-eye-contact-against-isolated-background_662251-838.jpg?t=st=1712896217~exp=1712899817~hmac=4889f4f7f534f3049e2f77fbdb49d65bd0f997c7d15dbd67d7f98de037cb7cd6&w=740',
+    title: 'Smiling young male professional standing',
+    link:'https://www.freepik.com/free-photo/smiling-young-male-professional-standing-with-arms-crossed-while-making-eye-contact-against-isolated-background_27507898.htm#fromView=search&page=1&position=0&uuid=6c210ec5-51d1-4315-885f-cc69ba2682b5',
+  },
+]
+
+export default data
